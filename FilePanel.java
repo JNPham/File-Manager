7965a1e15ae37d1cc39b9a350631b62033641405
JNPham <72328778+JNPham@users.noreply.github.com>
2021-05-08 03:45:22 +0700

    Update FilePanel.java

--- FilePanel.java
+++ FilePanel.java
@@ -1,31 +1,72 @@
 package cecs277.file.manager;
 
+import java.awt.Desktop;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.DefaultListModel;
 import javax.swing.GroupLayout;
 import javax.swing.JList;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
 import javax.swing.tree.DefaultTreeModel;
 
 public class FilePanel extends JPanel{
+    String currentDir;
+    File curFile;
+    Desktop desktop = Desktop.getDesktop();
     JList list = new JList();
     DefaultListModel model = new DefaultListModel();
-    
+    FilePopup popup = new FilePopup();
     private JScrollPane scrollpane = new JScrollPane();
     public int size=460;
-
+    
     public FilePanel(){
         list.setCellRenderer(new ListCellRenderer());
         list.setModel(model);
+        list.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e)  {check(e);}
+            @Override
+            public void mouseReleased(MouseEvent e) {check(e);}
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                File f;
+                if (list.getSelectedValue() != null) {
+                    f = new File((String)list.getSelectedValue());
+                    if (f.isFile())
+                    if(e.getClickCount()==2 && e.getButton() == MouseEvent.BUTTON1){
+                        try {
+                            desktop.open(new File((String)list.getSelectedValue()));
+                        } catch (IOException ex) {
+                            System.out.println(ex.toString());
+                        }
+                }}
+            }
+        });
+        
+        list.setFont( new Font("monospaced", Font.PLAIN, 12) );
         scrollpane.setViewportView(list);
         scrollpane.setPreferredSize(new Dimension(375,size));
         GroupLayout layout = new GroupLayout(this);
@@ -39,9 +80,9 @@ public class FilePanel extends JPanel{
         add(scrollpane);
         this.setDropTarget(new MyDropTarget());
         list.setDragEnabled(true);
-        //this.setPreferredSize(new Dimension(200,size));
     }
   
+//    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
     public void fillList(File dir) {
         File[] files;
         if (dir.listFiles() != null){
@@ -61,6 +102,81 @@ public class FilePanel extends JPanel{
         else
             model.clear();
     }
+    
+    public void setCurrentDir(String s) {
+        currentDir = s;
+        System.out.println("Current Dir: " + currentDir);
+    }
+
+    public void check(MouseEvent e) {
+        if (e.isPopupTrigger()) { //if the event shows the menu
+            list.setSelectedIndex(list.locationToIndex(e.getPoint())); //select the item
+            popup.show(list, e.getX(), e.getY()); //and show the menu
+            curFile = new File((String)list.getSelectedValue()) ;
+        }
+    }
+
+    public class RenameListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            Dialog2Way rename = new Dialog2Way(null, true);
+            rename.setTitle("Rename");
+            rename.setCurrentDir(currentDir);
+            rename.setFromField(curFile.getName());
+            rename.setVisible(true);
+            String toField = rename.getToField(); 
+            System.out.println("Change name to: " + toField);
+            Path source = Paths.get(curFile.getAbsolutePath());
+            try {
+                Files.move(source, source.resolveSibling(toField));
+            } catch (IOException ex) {
+                System.out.println("Fail to rename");
+            }
+            
+        }
+    }
+
+    public class CopyListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            Dialog2Way copy = new Dialog2Way(null, true);
+            copy.setTitle("Copying");
+            copy.setCurrentDir(currentDir);
+            copy.setFromField(curFile.getName());
+            copy.setVisible(true);
+            String toField = copy.getToField();
+            
+        }
+    }
+
+    public class DeleteListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+    
+    class FilePopup extends JPopupMenu {
+        public FilePopup() {
+            JMenuItem rename = new JMenuItem("Rename");
+            rename.addActionListener(new RenameListener());
+            JMenuItem copy = new JMenuItem("Copy");
+            copy.addActionListener(new CopyListener());
+            JMenuItem paste = new JMenuItem("Paste");
+            JMenuItem delete = new JMenuItem("Delete");
+            delete.addActionListener(new DeleteListener());
+
+            add(rename);
+            add(new JSeparator());
+            add(copy);
+            add(new JSeparator());
+            add(paste);
+            add(new JSeparator());
+            add(delete);
+       }
+    }
+    public void mousePressed(MouseEvent e)  {check(e);}
+    public void mouseReleased(MouseEvent e) {check(e);}
     
     class MyDropTarget extends DropTarget {
         public void drop(DropTargetDropEvent evt){
@@ -96,4 +212,5 @@ public class FilePanel extends JPanel{
         }
 
     }
+    
 }
