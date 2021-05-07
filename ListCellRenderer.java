a02d9fc71a2e9638c662ec42400ddbfeebcd9ec8
JNPham <72328778+JNPham@users.noreply.github.com>
2021-05-08 03:46:00 +0700

    Update ListCellRenderer.java

--- ListCellRenderer.java
+++ ListCellRenderer.java
@@ -2,12 +2,13 @@ package cecs277.file.manager;
 
 import java.awt.Component;
 import java.io.File;
+import java.text.SimpleDateFormat;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
 import javax.swing.UIManager;
 
 public class ListCellRenderer extends DefaultListCellRenderer{
-
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
     @Override
     public Component getListCellRendererComponent(
             JList list,
@@ -17,17 +18,18 @@ public class ListCellRenderer extends DefaultListCellRenderer{
             boolean expanded) {
 
         Component c = super.getListCellRendererComponent(list, value, index, selected, expanded);
-
         String str = (String) value;
-        if (str instanceof String) {
-            File file = new File(str);
-            if (file.isDirectory()) {
+        File f = new File(str);
+        if (f instanceof File) {
+            if (f.isDirectory()) {
                 setIcon(UIManager.getIcon("FileView.directoryIcon"));
+                setText(f.getName());
+
             }
             else {
                 setIcon(UIManager.getIcon("FileView.fileIcon"));
+                setText(String.format("%-50s %-20s %s byte", f.getName(), dateFormat.format(f.lastModified()), String.valueOf(f.length())));
             }
-            setText(file.getAbsolutePath());
         }
         return this;
     }
